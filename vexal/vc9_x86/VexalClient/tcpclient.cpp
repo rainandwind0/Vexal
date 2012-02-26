36407a332be882360771c72117c84cd6c110b475
unknown <Jake@Jake-PC.(none)>
2012-02-25 18:34:26 -0700

    Pre-merge commit

--- vexal/vc9_x86/VexalClient/tcpclient.cpp
+++ vexal/vc9_x86/VexalClient/tcpclient.cpp
@@ -10,6 +10,16 @@
 #include <winsock2.h>
 
 #elif defined(VLINUX)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
 #endif
 
 namespace vexal
@@ -155,6 +165,7 @@ namespace vexal
 			return;
 		}
 #elif defined(VLINUX)
+
 
 #endif
 		// Log that we've connected!
